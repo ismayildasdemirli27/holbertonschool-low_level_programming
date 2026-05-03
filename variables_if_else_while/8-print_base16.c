d81f543f0f1bbef0fb313944a87806ce6fede3b5
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-05-03 15:35:03 +0000

    Complete tasks 8 and 9, and fix file tracking

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@ int main(void)
 	int n;
 	char ch;
 
-	for (n = 0; n < 10; n++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar(n + '0');
+		putchar(n);
 	}
 	for (ch = 'a'; ch <= 'f'; ch++)
 	{
---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 separated by , 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
