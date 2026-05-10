9443ee914766607a3ac406281e39f8b1994cc312
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-05-10 06:12:13 +0000

    Task 5: Added print_sign function

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number
- * @n: The number to check
+ * print_sign - prints the sign of a number
+ * @n: the int to check
  *
- * Return: 1 and prints + if n > 0, 0 and prints 0 if n == 0,
- * -1 and prints - if n < 0
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,5 +8,6 @@ int _islower(int c);
 int print_sign(int n);
 int print_sign(int n);
 int _isalpha(int c);
+int print_sign(int n);
 
 #endif
