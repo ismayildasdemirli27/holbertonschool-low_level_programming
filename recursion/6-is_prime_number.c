85ec1112f6d2e83ea22aa825d28a75ae0268dc16
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-02 05:18:35 +0000

    Fix Betty style errors: trailing whitespace and line length

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * check_prime - Ədədin sadə olub-olmadığını yoxlayan köməkçi funksiya
- * @n: Yoxlanılacaq ədəd
- * @i: Bölünməni yoxlamaq üçün sayağac (iterator)
+ * check_prime - Checks if a number is prime recursively
+ * @n: Number to check
+ * @i: Iterator
  *
- * Return: Əgər sadə ədəddirsə 1, əks halda 0
+ * Return: 1 if prime, 0 otherwise
  */
 int check_prime(int n, int i)
 {
@@ -13,22 +13,20 @@ int check_prime(int n, int i)
 		return (1);
 	if (n % i == 0)
 		return (0);
-		
+
 	return (check_prime(n, i + 1));
 }
 
 /**
- * is_prime_number - Verilmiş ədədin sadə olub-olmadığını qaytarır
- * @n: Yoxlanılacaq ədəd
+ * is_prime_number - Returns if a number is prime
+ * @n: Number to check
  *
- * Return: Sadə ədəddirsə 1, mürəkkəbdirsə 0
+ * Return: 1 if prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-	/* 1 və 1-dən kiçik ədədlər sadə ədəd hesab edilmir */
 	if (n <= 1)
 		return (0);
-		
-	/* Yoxlamağa 2-dən başlayırıq, çünki hər ədəd 1-ə bölünür */
+
 	return (check_prime(n, 2));
 }
