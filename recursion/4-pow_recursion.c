29dae4e39df8eaad812aa6ad707db73cef26e556
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-04 20:24:13 +0000

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y.
- * @x: the base number.
- * @y: the exponent.
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: The base number.
+ * @y: The exponent.
  *
- * Return: x to the power of y, or -1 if y < 0.
+ * Return: x to the power of y, or -1 if y is negative.
  */
 int _pow_recursion(int x, int y)
 {
@@ -13,6 +13,5 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	
 	return (x * _pow_recursion(x, y - 1));
 }
