e056c46e04ff69bcc8909ed4aeeddd5ae4a32b54
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-06 20:25:33 +0000

    Fix Betty style formatting spaces to tabs

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,13 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
+	void *ptr;
 
-    ptr = malloc(b);
-    if (ptr == NULL)
-    {
-        exit(98);
-    }
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
 
-    return (ptr);
+	return (ptr);
 }
