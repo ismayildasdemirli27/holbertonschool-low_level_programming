ba0fb9d4fd48a1606d42fc6776ed77e3a62f5a46
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-06 20:28:43 +0000

    Task 1: Add string_nconcat function

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
+
+	while (i < len1)
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+
+	while (j < n)
+	{
+		concat[i++] = s2[j++];
+	}
+
+	concat[i] = '\0';
+
+	return (concat);
+}
