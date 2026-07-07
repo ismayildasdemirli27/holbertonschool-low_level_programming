5933341b3c3f28fdb34eb5968a524a20f589a20b
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-07 18:48:18 +0000

    Fix Betty style: removed long comments

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,24 +17,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		/* Növbəti arqumenti string (char *) kimi alırıq */
 		str = va_arg(args, char *);
 
-		/* Şərt: Əgər string NULL-dursa (nil) çap et, əks halda özünü çap et */
 		if (str == NULL)
-		{
 			printf("(nil)");
-		}
 		else
-		{
 			printf("%s", str);
-		}
 
-		/* Şərt: Ayırıcı NULL deyilsə və sonuncu sətirdə deyiliksə, ayırıcını çap et */
 		if (separator != NULL && i < n - 1)
-		{
 			printf("%s", separator);
-		}
 	}
 
 	printf("\n");
