ff6a9ef75d89d54fe8f361dc047e9767e6f48684
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-07 17:46:26 +0000

    4. Free list

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -18,14 +18,10 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-/* Task 0: Print list */
 size_t print_dlistint(const dlistint_t *h);
-
-/* Task 1: List length */
 size_t dlistint_len(const dlistint_t *h);
-
-/* Task 2: Add node */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
 
 #endif /* LISTS_H */
