818feb990b788b05bcdb09b16c5baff3a73ad75a
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-07 18:09:34 +0000

    Fix Betty line length in Task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,7 +4,7 @@
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: double pointer to the head of the list
- * @idx: index where the new node should be added (starts at 0)
+ * @idx: index where the new node should be added
  * @n: data for the new node
  *
  * Return: address of the new node, or NULL if it failed
@@ -17,26 +17,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 
-	/* Əgər indeks 0-dırsa, birbaşa əvvələ əlavə edən funksiyanı çağırırıq */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	/* İndeksdən bir əvvəlki (idx - 1) düyünü tapırıq */
 	while (temp != NULL && i < idx - 1)
 	{
 		temp = temp->next;
 		i++;
 	}
 
-	/* Əgər temp NULL oldusa, deməli indeks siyahının limitini aşıb */
 	if (temp == NULL)
 		return (NULL);
 
-	/* Əgər temp sonuncu düyündürsə, birbaşa sona əlavə edən funksiyanı çağırırıq */
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	/* Ortaya əlavə etmək üçün yaddaş ayırırıq */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
