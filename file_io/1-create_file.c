f2b272ff3f1e8d1bbfe7408ecdac6a779eacf9a1
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-09 07:34:15 +0000

    Fix Betty style for 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,20 +18,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* Mətnin uzunluğunu tapırıq (əgər NULL deyilsə) */
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
 
-	/* Faylı yaradırıq (O_CREAT), yazmaq üçün açırıq (O_WRONLY), 
-	   əgər varsa içini silirik (O_TRUNC). İcazə: 0600 (rw-------) */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	/* Əgər text_content NULL deyilsə, fayla yazırıq */
 	if (text_content != NULL)
 	{
 		n_written = write(fd, text_content, len);
