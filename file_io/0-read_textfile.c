3a56e9f095063b66671e822d425a2453c4c0789c
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-09 07:29:02 +0000

    Fix empty function in 0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,9 +1,54 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-#include <stddef.h>
-#include <sys/types.h>
+/**
+ * read_textfile - Reads a text file and prints it to POSIX standard output.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters it should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ * Otherwise - the actual number of bytes it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_written;
+	char *buffer;
 
-ssize_t read_textfile(const char *filename, size_t letters);
+	if (filename == NULL)
+		return (0);
 
-#endif
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (n_written);
+}
